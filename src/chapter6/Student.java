2a850f91589bde270f1772868ecd72ada068dbe7
YeJun <hayejun1013@naver.com>
2022-08-03 10:31:34 +0900

    chore: cooperation between object study notes

---
+++ src/chapter6/Student.java
@@ -0,0 +1,26 @@
+package chapter6;
+
+public class Student {
+    String name;
+    int grade;
+    int money;
+
+    public Student(String name, int money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public void takeBus(Bus bus) {
+        bus.take(1000);
+        money -= 1000;
+    }
+
+    public void takeSubway(Subway subway) {
+        subway.take(1200);
+        money -= 1200;
+    }
+
+    public void showInfo() {
+        System.out.println(name + "님의 남은 돈은 " + money + "입니다.");
+    }
+}
---
+++ src/chapter6/Subway.java
@@ -0,0 +1,21 @@
+package chapter6;
+
+public class Subway {
+    int number;
+    int passengerCount;
+    int money;
+
+    public Subway(int number) {
+        this.number = number;
+    }
+
+    public void take(int money) {
+        passengerCount++;
+        this.money += money;
+    }
+
+    public void showInfo() {
+        System.out.println("지하철 " + number + "번의 승객은 " + passengerCount +
+                "명이고, 수입은 " + money + "입니다");
+    }
+}
