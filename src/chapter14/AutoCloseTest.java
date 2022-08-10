997ae9b958a453afff5a57848081b78638ad0b56
YeJun <hayejun1013@naver.com>
2022-08-10 10:57:48 +0900

    chore: exception handling study notes

---
+++ src/chapter14/AutoCloseTest.java
@@ -0,0 +1,12 @@
+package chapter14;
+
+public class AutoCloseTest {
+    public static void main(String[] args) {
+        AutoCloseObj obj = new AutoCloseObj();
+        try (obj) {
+            throw new Exception();
+        } catch (Exception e) {
+            System.out.println("exception");
+        }
+    }
+}
